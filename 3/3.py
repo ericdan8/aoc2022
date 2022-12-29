4c1a852d53f790967cbbc361780cd7db8c148abd
ericdan <eric.dan8@gmail.com>
2022-12-29 12:19:04 -0500

    d3p2

--- 3/3.py
+++ 3/3.py
@@ -1,12 +1,16 @@
 with open("input", "r") as f:
     lines = f.readlines()
+    lines = [l.strip() for l in lines]
+
+groups = [lines[i*3:(i*3)+3] for i in range(len(lines) // 3)]
 
 score = 0
-for l in lines:
-    l = l.strip()
-    x = set(c for c in l[:len(l) // 2])
-    y = set(c for c in l[len(l) // 2:])
-    common = x.intersection(y)
+for g in groups:
+    print(g)
+    [x, y, z] = [set([c for c in e]) for e in g]
+
+    common = x.intersection(y).intersection(z)
+    print(common)
     common = common.pop()[0]
 
     if common.isupper():
