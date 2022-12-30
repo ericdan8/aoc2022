fe5dbc1275b016876bb990145eae55d6cf3e0a64
ericdan <eric.dan8@gmail.com>
2022-12-30 12:34:38 -0500

    d9p2

--- 9/9.py
+++ 9/9.py
@@ -1,36 +1,42 @@
 with open("input", "r") as f:
     lines = [l.strip() for l in f.readlines()]
 
-head_x = 0
-head_y = 0
-tail_x = 0
-tail_y = 0
+rope_x = [0] * 10
+rope_y = [0] * 10
 visited = set()
 
 for l in lines:
     d, n = l.split()
+    # print(f"{d} {n}")
     for i in range(int(n)):
+        # print(d)
         if d == "R":
-            head_x += 1
+            rope_x[0] += 1
         elif d == "L":
-            head_x -= 1
+            rope_x[0] -= 1
         elif d == "U":
-            head_y += 1
+            rope_y[0] += 1
         elif d == "D":
-            head_y -= 1
+            rope_y[0] -= 1
         
-        d_x = head_x - tail_x
-        d_y = head_y - tail_y
+        for j in range(1, len(rope_x)):
+            ahead_x = rope_x[j-1]
+            ahead_y = rope_y[j-1]
 
-        if abs(d_x) >= 2:
-            dn = 1 if d_x > 0 else -1
-            tail_x += dn
-            tail_y = head_y
-        elif abs(d_y) >= 2:
-            dn = 1 if d_y > 0 else -1
-            tail_y += dn
-            tail_x = head_x
+            d_x = ahead_x - rope_x[j]
+            d_y = ahead_y - rope_y[j]
+
+            if abs(d_x) >= 2 or abs(d_y) >= 2:
+                dn_x = d_x // (abs(d_x) if abs(d_x) > 0 else 1)
+                dn_y = d_y // (abs(d_y) if abs(d_y) > 0 else 1)
+                rope_x[j] += dn_x
+                rope_y[j] += dn_y
+            # elif abs(d_y) >= 2:
+            #     dn = 1 if d_y > 0 else -1
+            #     rope_y[j] += dn
+            #     rope_x[j] = ahead_x
+            
+        visited.add((rope_x[-1], rope_y[-1]))
         
-        visited.add((tail_x, tail_y))
 
 print(len(visited))
